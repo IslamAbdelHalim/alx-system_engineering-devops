a57979e66265346b49479ec66a07700a096647be
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-03-26 00:40:43 +0200

    executed when imported

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,29 +7,29 @@ returns information about his/her TODO list progress.
 import requests
 from sys import argv
 
+if __name__ == "__main__":
+    req_user = requests.get(
+            "https://jsonplaceholder.typicode.com/users/{}".format(argv[1]))
+    user_todo = requests.get(
+            "https://jsonplaceholder.typicode.com/todos/")
 
-req_user = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}".format(argv[1]))
-user_todo = requests.get(
-        "https://jsonplaceholder.typicode.com/todos/")
+    employee_name = req_user.json().get("username")
+    number_tasks = 0
+    total_tasks = 0
 
-employee_name = req_user.json().get("username")
-number_tasks = 0
-total_tasks = 0
+    for x in user_todo.json():
+        if x.get("userId") == int(argv[1]):
+            total_tasks += 1
+            if x.get("completed"):
+                number_tasks += 1
 
-for x in user_todo.json():
-    if x.get("userId") == int(argv[1]):
-        total_tasks += 1
-        if x.get("completed"):
-            number_tasks += 1
-
-employee = "Employee {} is done with tasks({}/{}):".format(
+    employee = "Employee {} is done with tasks({}/{}):".format(
                                                         employee_name,
                                                         number_tasks,
                                                         total_tasks)
-print(employee)
+    print(employee)
 
-for x in user_todo.json():
-    if x.get("userId") == int(argv[1]):
-        if x.get("completed") is True:
-            print("\t {}".format(x.get("title")))
+    for x in user_todo.json():
+        if x.get("userId") == int(argv[1]):
+            if x.get("completed") is True:
+                print("\t {}".format(x.get("title")))
