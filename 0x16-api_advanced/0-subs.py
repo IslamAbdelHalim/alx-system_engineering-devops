db60a25259e6ba7bf3f0677065721a55295d6543
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-04-11 23:51:59 +0200

    a

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """function to get number of subscribers in reddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
-    data = requests.get(url)
+    data = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 
     if data.status_code != 200:
         return 0
